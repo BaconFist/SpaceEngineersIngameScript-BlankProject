300655392c09b5d7f8d36b769c8870335bdd96a8
Thomas Klose <thomas@bratler.net>
2016-06-07 02:53:05 +0200

    BlankProjectTemplate

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # SpaceEngineersIngameScript
+
+#### A Visual Studio Project for SpaceEngineers Ingame Programming
+
+* IntelliSense support
+* blank Template for in-game Scripts
+
+## Project setup
+
+
+1. Download/Clone this Repository
+2. Open the Solution in Visual Studio
+3. **(Chnage path to Gamelibraries if SpaceEngineers is not installed in the default location):** Navigate `Project`->`SpaceEngineersIngameScript Properties...`->`Reference Paths` and update the path to the gamelibraries
+4. Navigate to `File`->`Export`
+5. Confirm saving changes
+6. Select `Item Template` and `SpaceEngineersIngameScript`
+7. On the next page Select **only** `IngameScript.cs`
+8. skip to the end of the wizzard and hit `Finish`
+9. Close Visual studio.
+
+If everything went well you will have IntelliSense support and you can add an empty script using `Project`->`new Item`
+
+## Change the Template
+
+To change the Template edit `Template\IngameScript.cs` to ypur needs and repeat steps 4 to 8 from Project setup. You may need to restart Visual Studio for the changes to take place.
---
+++ SpaceEngineersIngameScript/Template/IngameScript.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using VRageMath; // VRage.Math.dll
+using VRage.Game; // VRage.Game.dll
+using System.Text;
+using Sandbox.ModAPI.Interfaces; // Sandbox.Common.dll
+using Sandbox.ModAPI.Ingame; // Sandbox.Common.dll
+using Sandbox.Game.EntityComponents; // Sandbox.Game.dll
+using VRage.Game.Components; // VRage.Game.dll
+using VRage.Collections; // VRage.Library.dll
+using VRage.Game.ObjectBuilders.Definitions; // VRage.Game.dll
+using VRage.Game.ModAPI.Ingame; // VRage.Game.dll
+using SpaceEngineers.Game.ModAPI.Ingame; // SpacenEngineers.Game.dll
+
+namespace $safeitemname$ {
+    public class Program : MyGridProgram
+    {
+        #region Game Code - Copy/Paste Code from this region into Block Script Window in Game
+
+        public Program()
+        {
+
+            // The constructor, called only once every session and
+            // always before any other method is called. Use it to
+            // initialize your script. 
+            //     
+            // The constructor is optional and can be removed if not
+            // needed.
+
+        }
+
+        public void Save()
+        {
+
+            // Called when the program needs to save its state. Use
+            // this method to save your state to the Storage field
+            // or some other means. 
+            // 
+            // This method is optional and can be removed if not
+            // needed.
+
+        }
+
+        public void Main(string argument)
+        {
+            // The main entry point of the script, invoked every time
+            // one of the programmable block's Run actions are invoked.
+            // 
+            // The method itself is required, but the argument above
+            // can be removed if not needed.
+        }
+        #endregion End of  Game Code - Copy/Paste Code from this region into Block Script Window in Game
+    }
+}
